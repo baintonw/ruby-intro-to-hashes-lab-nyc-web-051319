1aaead32afcce4cb3e7b7b935eb7c88046ce79db
William Bainton <baintonw@gmail.com>
2019-05-05 19:39:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def new_hash
  {}
 end
@@ -46,6 +47,7 @@ def monopoly_with_fourth_tier
 	        :pennsylvania_railroad => {"mortgage_value" => "$200"},
 	        :b_and_o_railroad => {"mortgage_value" "400"},
 	        :shortline => {"mortgage_value" => "$800"}
+	        binding.pry
 	    },
 	   :rent_in_dollars => {
 	     :one_piece_owned => 25,
